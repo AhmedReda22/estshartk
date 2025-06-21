3b1b4c481ff905edef19d1673799aa0c3de76606
AhmedReda22 <ahmed.mo.reda215@gmail.com>
2025-06-21 12:58:26 +0300

    update2

--- src/App.js
+++ src/App.js
@@ -1,36 +1,29 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useLocation
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
-import Dashboard from "./components/Dashboard/Dashboard";
+import Dashboard from "./components/Dashboard/Dashboard"; // ✅ Import the Dashboard
 import "bootstrap-icons/font/bootstrap-icons.css";
 
-import { library } from "@fortawesome/fontawesome-svg-core";
-import {
-  faPhone,
-  faHome,
-  faUser,
-  faBars,
-  faTimes
-} from "@fortawesome/free-solid-svg-icons";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faPhone, faHome, faUser, faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 library.add(faPhone, faHome, faUser, faBars, faTimes);
 
-// Layout component handles showing/hiding Navbar
-function Layout({ children }) {
+// ✅ Layout component handles showing/hiding Navbar
+function Layout() {
   const location = useLocation();
   const hideNavbar = location.pathname.startsWith("/dashboard");
 
   return (
     <>
       {!hideNavbar && <Navbar />}
-      {children}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
     </>
   );
 }
@@ -38,13 +31,7 @@ function Layout({ children }) {
 export default function App() {
   return (
     <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </Layout>
+      <Layout />
     </Router>
   );
 }
